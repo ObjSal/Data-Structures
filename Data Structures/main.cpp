9422b4cdbf6ab8db847db375f26c185fc12b5418
Salvador Guerrero <salvador.icc@gmail.com>
2016-03-15 20:22:21 -0700

    Lists gist added

--- Data Structures/main.cpp
+++ Data Structures/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include "List.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void list_test()
+{
+    struct list *list = (struct list *)calloc(1, sizeof(struct list));
+    list->item = 10;
+
+    // insert
+
+    list_insert(&list, 11);
+    list_insert(&list, 12);
+    list_insert(&list, 13);
+    list_insert(&list, 14);
+    list_insert(&list, 15);
+
+    list_delete(&list, 13);
+
+    // will print in inverse because insert happens in the head of the list.
+
+    list_description(list);
+
+    list_free(list);
+}
+
+int main(int argc, const char * argv[])
+{
+    list_test();
+
     return 0;
 }
