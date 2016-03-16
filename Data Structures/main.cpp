e8f8f1906d275350ed985ad325112cd3a75ee408
Salvador Guerrero <salvador.icc@gmail.com>
2016-03-15 21:20:31 -0700

    Stack submodule added

--- Data Structures/main.cpp
+++ Data Structures/main.cpp
@@ -8,32 +8,51 @@
 
 #include <iostream>
 #include "List.h"
+#include "Stack.h"
 
-void list_test()
+void stack_test(Stack **stack)
 {
-    struct list *list = (struct list *)calloc(1, sizeof(struct list));
-    list->item = 10;
+    printf("\nstack test:\n");
 
-    // insert
+    stack_push(stack, 16);
+    stack_push(stack, 17);
+    stack_push(stack, 18);
+    stack_push(stack, 19);
+    stack_push(stack, 20);
 
-    list_insert(&list, 11);
-    list_insert(&list, 12);
-    list_insert(&list, 13);
-    list_insert(&list, 14);
-    list_insert(&list, 15);
+    Stack *poppped_node = stack_pop(stack);
+    printf("poppped_node: %d\n", poppped_node->item);
+    list_free(poppped_node);
 
-    list_delete(&list, 13);
+    list_description(*stack);
+}
+
+void list_test(struct list **list)
+{
+    printf("\nlist test:\n");
+
+    list_insert(list, 11);
+    list_insert(list, 12);
+    list_insert(list, 13);
+    list_insert(list, 14);
+    list_insert(list, 15);
+
+    list_delete(list, 13);
 
     // will print in inverse because insert happens in the head of the list.
 
-    list_description(list);
-
-    list_free(list);
+    list_description(*list);
 }
 
 int main(int argc, const char * argv[])
 {
-    list_test();
+    struct list *list = (struct list *)calloc(1, sizeof(struct list));
+    list->item = 10;
+
+    list_test(&list);
+    stack_test(&list);
+
+    list_free(list);
 
     return 0;
 }
