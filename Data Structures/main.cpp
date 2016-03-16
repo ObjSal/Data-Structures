5fad6e153e47d46193041427c3d07349f05b41e5
Salvador Guerrero <salvador.icc@gmail.com>
2016-03-15 19:22:03 -0700

    Initial Commit

---
+++ Data Structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structures
+//
+//  Created by Salvador Guerrero on 3/15/16.
+//  Copyright © 2016 ByteApps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
